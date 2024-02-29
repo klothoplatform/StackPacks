d7dfd45e7250eab4c01746aeec3e85bb9732ceee
Jordan Singer <jordan.singer@klo.dev>
2024-02-29 16:29:01 -0600

    add injection

--- src/dependencies/injection.py
+++ src/dependencies/injection.py
@@ -1,6 +1,16 @@
 from dataclasses import dataclass
 import boto3
+import os
+
+from src.stack_pack.storage.iac_storage import IacStorage
 
 
 def create_sts_client():
     return boto3.client("sts")
+
+def create_iac_bucket():
+    resource = boto3.resource("s3")
+    return resource.Bucket(os.environ.get("IAC_BUCKET"))
+
+def get_iac_storage():
+    return IacStorage(create_iac_bucket())
