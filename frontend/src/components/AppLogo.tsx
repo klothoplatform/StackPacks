c608bbccd9802644bb64e2ea01fc18b38cbf0714
DavidSeptimus-Klotho <david.septimus@klo.dev>
2024-03-12 20:10:25 -0600

    Adds more logos
    Added logos for strapi, supertokens, metabase, and typesense

--- frontend/src/components/AppLogo.tsx
+++ frontend/src/components/AppLogo.tsx
@@ -1,11 +1,16 @@
 import gitea from "/images/logos/gitea.svg";
 import mattermost from "/images/logos/mattermost.svg";
-import mattermostWhite from "/images/logos/mattermost-white.svg";
+import mattermostDark from "/images/logos/mattermost-dark.svg";
 import gitness from "/images/logos/gitness.svg";
-import gitnessWhite from "/images/logos/gitness-white.svg";
+import gitnessDark from "/images/logos/gitness-dark.svg";
 import tooljet from "/images/logos/tooljet.svg";
 import type { FC, ImgHTMLAttributes } from "react";
 import { twMerge } from "tw-merge";
+import metabase from "/images/logos/metabase.svg";
+import strapi from "/images/logos/strapi.svg";
+import supertokens from "/images/logos/supertokens.svg";
+import supertokensDark from "/images/logos/supertokens-dark.svg";
+import typesense from "/images/logos/typesense.png";
 
 export interface LogoMapping {
   filePath: string;
@@ -17,14 +22,21 @@ const LogoMappings: Record<string, string | LogoMapping> = {
   gitea,
   gitness: {
     filePath: gitness,
-    darkPath: gitnessWhite,
+    darkPath: gitnessDark,
     className: "h-8",
   },
   mattermost: {
     filePath: mattermost,
-    darkPath: mattermostWhite,
+    darkPath: mattermostDark,
   },
   tooljet,
+  metabase,
+  strapi,
+  supertokens: {
+    filePath: supertokens,
+    darkPath: supertokensDark,
+  },
+  typesense,
 };
 
 export const AppLogo: FC<
