862c2228e70e8ef13116e9b76ebccf2005f76b5b
Jordan Singer <jordan.singer@klo.dev>
2024-03-19 12:30:16 -0500

    remove comments

--- scripts/cli.py
+++ scripts/cli.py
@@ -1,25 +1,7 @@
-# import anyio as _
-# import asyncclick as click
-
 import asyncclick as click
 
 from scripts.dynamodb import dynamodb
 from scripts.iac_generator import iac
-
-# @click.group()
-# async def cli():
-#     pass
-
-# cli.add_command(dynamodb)
-# cli.add_command(iac)
-
-# if __name__ == "__main__":
-
-#     cli()
-
-
-# You can use all of click's features as per its documentation.
-# Async commands are supported seamlessly; they just work.
 
 
 @click.group()
@@ -27,28 +9,7 @@ async def cli():
     pass
 
 
-# @click.group()
-# async def dynamodb():
-#     pass
-
-# @dynamodb.command()
-# @click.option("--count", default=1, help="Number of greetings.")
-# @click.option("--name", prompt="Your name", help="The person to greet.")
-# async def hello(count, name):
-#     """Simple program that greets NAME for a total of COUNT times."""
-#     for x in range(count):
-#         if x: await anyio.sleep(0.1)
-#         click.echo(f"Hello, {name}!")
-
 if __name__ == "__main__":
     cli.add_command(dynamodb)
     cli.add_command(iac)
-    cli(_anyio_backend="asyncio")  # or asyncio
-
-# You can use your own event loop:
-#    import anyio
-#    async def main():
-#        await hello.main()
-#    if __name__ == '__main__':
-#        anyio.run(main)
-# This is useful for testing.
+    cli(_anyio_backend="asyncio")
