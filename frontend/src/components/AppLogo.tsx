b7ff7b51080cc1786d03ee915da40f6b1798a7b5
DavidSeptimus-Klotho <david.septimus@klo.dev>
2024-03-21 13:59:33 -0600

    Adds more logos
    Added logos for:
    - Cal.com
    - Keila
    - Matomo
    - OwnCloud
    - Rallly

--- frontend/src/components/AppLogo.tsx
+++ frontend/src/components/AppLogo.tsx
@@ -1,15 +1,20 @@
+import type { FC, ImgHTMLAttributes } from "react";
+import { twMerge } from "tw-merge";
 import Gitea from "/images/logos/gitea.svg";
 import Mattermost from "/images/logos/mattermost.svg";
 import mattermostDark from "/images/logos/mattermost-dark.svg";
 import Gitness from "/images/logos/gitness.svg";
 import GitnessDark from "/images/logos/gitness-dark.svg";
 import ToolJet from "/images/logos/tooljet.svg";
-import type { FC, ImgHTMLAttributes } from "react";
-import { twMerge } from "tw-merge";
 import Metabase from "/images/logos/metabase.svg";
 import Strapi from "/images/logos/strapi.svg";
 import SuperTokens from "/images/logos/supertokens.svg";
 import SuperTokensDark from "/images/logos/supertokens-dark.svg";
+import CalCom from "/images/logos/cal-com.svg";
+import Keila from "/images/logos/keila.svg";
+import Matomo from "/images/logos/matomo.svg";
+import OwnCloud from "/images/logos/owncloud.svg";
+import Rallly from "/images/logos/rallly.svg";
 import TypeSense from "/images/logos/typesense.png";
 
 export interface LogoMapping {
@@ -19,24 +24,29 @@ export interface LogoMapping {
 }
 
 const LogoMappings: Record<string, string | LogoMapping> = {
-  Gitea,
-  Gitness: {
+  calcom: CalCom,
+  gitea: Gitea,
+  gitness: {
     filePath: Gitness,
     darkPath: GitnessDark,
     className: "h-8",
   },
-  Mattermost: {
+  keila: Keila,
+  matomo: Matomo,
+  owncloud: OwnCloud,
+  mattermost: {
     filePath: Mattermost,
     darkPath: mattermostDark,
   },
-  ToolJet,
-  Metabase,
-  Strapi,
-  SuperTokens: {
+  tooljet: ToolJet,
+  metabase: Metabase,
+  rallly: Rallly,
+  strapi: Strapi,
+  supertokens: {
     filePath: SuperTokens,
     darkPath: SuperTokensDark,
   },
-  TypeSense,
+  typesense: TypeSense,
 };
 
 export const AppLogo: FC<
