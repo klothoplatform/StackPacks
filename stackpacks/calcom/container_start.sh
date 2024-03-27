642f76f9b7c0a10bfc6427e68042b85565e16724
Gordon <gordon.goetz@klo.dev>
2024-03-27 10:30:10 -0400

    Fixes for new apps

--- stackpacks/calcom/container_start.sh
+++ stackpacks/calcom/container_start.sh
@@ -3,4 +3,6 @@
 export DATABASE_URL=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${DATABASE_ENDPOINT}/${POSTGRES_DB}
 export DATABASE_DIRECT_URL=${DATABASE_URL}
 
+export NEXT_PUBLIC_WEBAPP_URL=http://${DNS_NAME}
+
 exec "/calcom/scripts/start.sh"
--- stackpacks/rallly/container_start.sh
+++ stackpacks/rallly/container_start.sh
@@ -1,5 +1,7 @@
 #/usr/bin/sh
 
-export DATABASE_URL="postgresql://${RALLY_DB_USER}:${RALLY_DB_PASSWORD}@${RALLY_DB_ENDPOINT}/${RALLY_DB_NAME}"
+export DATABASE_URL="postgres://${RALLLY_DB_USER}:${RALLLY_DB_PASSWORD}@${RALLLY_DB_ENDPOINT}/${RALLLY_DB_NAME}"
+export NEXT_PUBLIC_BASE_URL="http://${DOMAIN_NAME}"
+export NEXT_PUBLIC_APP_BASE_URL="http://${DOMAIN_NAME}"
 
 exec ./docker-start.sh
