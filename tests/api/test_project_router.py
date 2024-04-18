a2de93ddd90dd5e423ed114ea01cb0813e7a3c5f
Gordon <gordon.goetz@klo.dev>
2024-04-18 18:42:40 -0400

    Add test case for update_stack

--- tests/api/test_project_router.py
+++ tests/api/test_project_router.py
@@ -8,17 +8,21 @@ from src.api.project_router import (
     AppRequest,
     StackRequest,
     StackResponse,
+    UpdateStackRequest,
     add_app,
     create_stack,
     remove_app,
     update_app,
+    update_stack,
 )
 from src.project.models.app_deployment import AppDeployment
 from src.project.models.project import Project, ProjectView
 from src.util.aws.iam import Policy
+from tests.test_utils.pynamo_test import PynamoTest
 
 
-class TestProjectRoutes(aiounittest.AsyncTestCase):
+class TestProjectRoutes(PynamoTest, aiounittest.AsyncTestCase):
+    models = [Project, AppDeployment]
 
     @patch("src.api.project_router.get_binary_storage")
     @patch("src.api.project_router.get_stack_packs")
@@ -128,6 +132,72 @@ class TestProjectRoutes(aiounittest.AsyncTestCase):
         mock_pack.run_common_pack.assert_not_called()
         mock_pack.run_packs.assert_not_called()
         mock_pack.save.assert_not_called()
+
+    @patch("src.api.project_router.get_binary_storage")
+    @patch("src.api.project_router.get_stack_packs")
+    @patch("src.api.project_router.get_user_id")
+    @patch("src.api.project_router.Project")
+    @patch("src.api.project_router.TempDir")
+    async def test_update_stack(
+        self,
+        mock_tmp_dir,
+        mock_project,
+        mock_get_user_id,
+        mock_get_stack_packs,
+        mock_get_binary_storage,
+    ):
+        mock_get_user_id.return_value = "user_id"
+        project_view = ProjectView(
+            id="id", owner="user_id", created_by="user_id", created_at=0
+        )
+        mock_project_instance = MagicMock(
+            spec=Project,
+            id="user_id",
+            to_view_model=MagicMock(return_value=project_view),
+        )
+        mock_project.get.return_value = mock_project_instance
+
+        mock_project_instance.get_policy.return_value = "policy"
+        sps = {"app1": MagicMock(), "app2": MagicMock()}
+        mock_get_stack_packs.return_value = sps
+        binary_storage = mock_get_binary_storage.return_value
+        mock_tmp_dir.return_value.__enter__.return_value = "/tmp"
+
+        app = AppDeployment(
+            project_id=mock_project_instance.id,
+            range_key=AppDeployment.compose_range_key("app1", 1),
+            created_by="user_id",
+            status="",
+            configuration={"config1": "value0"},
+        )
+        app.save()
+
+        response: StackResponse = await update_stack(
+            MagicMock(),
+            UpdateStackRequest(configuration={"app1": {"config1": "value1"}}),
+        )
+
+        mock_get_user_id.assert_called_once()
+        mock_project.get.assert_called_once_with("user_id")
+
+        mock_get_stack_packs.assert_called_once()
+        mock_tmp_dir.assert_called_once()
+        mock_tmp_dir.return_value.__enter__.assert_called_once()
+        mock_project_instance.run_base.assert_called_once_with(
+            stack_packs=[sps["app1"]],
+            config={},
+            binary_storage=binary_storage,
+            tmp_dir="/tmp",
+        )
+        mock_project_instance.run_pack.assert_called_once_with(
+            stack_packs=sps,
+            config={"app1": {"config1": "value1"}},
+            binary_storage=binary_storage,
+            tmp_dir="/tmp",
+        )
+        mock_project_instance.get_policy.assert_called_once()
+        self.assertEqual(response.stack, project_view)
+        self.assertEqual(response.policy, "policy")
 
     @patch("src.api.project_router.get_binary_storage")
     @patch("src.api.project_router.get_stack_packs")
