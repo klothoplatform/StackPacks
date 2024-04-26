37ef73e2275baa73f9358c273182a02c2d0e5193
DavidSeptimus-Klotho <david.septimus@klo.dev>
2024-04-25 23:45:02 -0400

    Added disabled variant for flowbite dropdown items

--- frontend/src/flowbite-theme.ts
+++ frontend/src/flowbite-theme.ts
@@ -130,7 +130,7 @@ const flowbiteTheme: CustomFlowbiteTheme = {
       hidden: "invisible opacity-0",
       item: {
         container: "",
-        base: "flex w-full cursor-pointer items-center justify-start px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:bg-gray-600 dark:focus:text-white",
+        base: "flex w-full cursor-pointer items-center justify-start px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:bg-gray-600 dark:focus:text-white disabled:text-gray-400 disabled:dark:text-gray-500 disabled:italic",
         icon: "mr-2 h-4 w-4",
       },
       style: {
